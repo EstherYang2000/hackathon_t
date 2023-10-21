0ee0b4fd7b80d9437d1ac32d4db3fcf060491682
esther <ester6126@gmail.com>
2023-10-21 10:42:08 +0800

    modify xray scan

--- backend/app/routes/__init__.py
+++ backend/app/routes/__init__.py
@@ -5,3 +5,4 @@ bp = Blueprint('main', __name__)
 from .entry import upload_image
 from .security import security_dashboard,security_chart
 from .attend import security_chart1
+from .anaysis import analysis_xray
