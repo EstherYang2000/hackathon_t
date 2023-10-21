39b946656a61b21d61fde1c941c0ff6b897c1fee
Vincent826826 <vincent826826@gmail.com>
2023-10-21 11:51:32 +0800

    revised analysis.py output format

--- backend/app/routes/anaysis.py
+++ backend/app/routes/anaysis.py
@@ -14,7 +14,11 @@ from datetime import datetime
 
 @bp.route('/analysis', methods=['GET'])
 def analysis_xray():
-        result_dicts = {z:{"real":[], "predict":[]} for z in ['AZ', 'HQ']}
+        result_dicts = {}
+        for ty in ["real", "predict"]:
+             for z in ['AZ', 'HQ']:
+                  result_dicts[z+'_'+ty] = []
+
         for z in ['AZ', 'HQ']:
             cursor = conn.cursor()
             with conn.cursor() as cur:
@@ -25,7 +29,7 @@ def analysis_xray():
                 cur.execute(sql)
                 data = cur.fetchall()
                 for row in data:
-                    result_dicts[z]["real"].append({"scanTime": float(row[1]), "dateTime": str(row[0])})
+                    result_dicts[z+"_real"].append({"scanTime": float(row[1]), "dateTime": str(row[0])})
 
                 data = pd.read_csv(os.path.join("app/routes/model_output/{}_ScanTime_Pred.csv".format(z)))
                 data.columns = ["date", "y_pred"]
@@ -34,7 +38,7 @@ def analysis_xray():
                     date_str = data.iloc[i]["date"]
                     date_obj = datetime.strptime(date_str, '%Y-%m-%d')
                     formatted_date = date_obj.strftime('%Y-%m-%d %H:%M:%S')
-                    result_dicts[z]["predict"].append({"scanTime": data.iloc[i]["y_pred"], "dateTime": formatted_date})
+                    result_dicts[z+"_predict"].append({"scanTime": data.iloc[i]["y_pred"], "dateTime": formatted_date})
                 # print(data.iloc[0]["date"])
                 
         return result_dicts
