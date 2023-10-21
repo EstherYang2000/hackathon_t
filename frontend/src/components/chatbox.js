fbf1c0c1d96ba39f1e0091c2a277e40c20058c15
ty-my-account <ty8484920a@gmail.com>
2023-10-21 22:36:47 +0800

    fix chatbox bug

--- frontend/src/components/chatbox.js
+++ frontend/src/components/chatbox.js
@@ -1,22 +1,24 @@
-import React , { useEffect, useState, useRef } from "react"
+import React, { useEffect, useState, useRef } from "react";
 // import { Row , Col } from "react-bootstrap"
-import { Row, Col } from 'antd';
+import { Row, Col } from "antd";
 
-import {ChatFeed, ChatBubble, Message} from 'react-chat-ui'
+import { ChatFeed, ChatBubble, Message } from "react-chat-ui";
 // import Sidebar from "../../component/Sidebar"
 import "../styles/chat.css";
 import user_icon from "../imgs/user.png";
 import Iris from "../imgs/IRIS.png";
-import send from "../imgs/send.png"
+import send from "../imgs/send.png";
 import api from "../utils/api";
 
 // socket
-const endpoint = "http://140.117.71.98:4001"
+const endpoint = "http://140.117.71.98:4001";
 // const socket = socketIOClient(endpoint);
 
 function Chatbox() {
-  const [message, setMessage] = useState([new Message({id:1 ,message: 'Hello World!'})])
-  
+  const [message, setMessage] = useState([
+    new Message({ id: 1, message: "Hello World!" }),
+  ]);
+
   // const [current, setCurrent] = useState(1);
   let input = useRef();
 
@@ -30,58 +32,70 @@ function Chatbox() {
     // api code
     // get dialog key : localStorage.getItem('key');
     const api_data = {
-      'zone' : "HQ",
-      "start_date" : "2023-09-18",
-      "end_date" : "2023-09-24",
-      "dept" : "DEPT4",
-      "conversation_id" : localStorage.getItem("conversation_id"),
+      zone: "HQ",
+      start_date: "2023-09-18",
+      end_date: "2023-09-24",
+      dept: "DEPT4",
+      conversation_id: localStorage.getItem("conversation_id"),
+      input: data.value,
     };
-    api.post("/hr/dashboard/llmrealtime/",api_data).then(res => {
+    api.post("/hr/dashboard/llmrealtime/", api_data).then((res) => {
       console.log(res);
-    })
+    });
     // setCurrent(1)
-    data.value = '';
+    data.value = "";
     return true;
-  }
+  };
 
-  useEffect(()=>{
-    localStorage.setItem('conversation_id', 0)
-  })
+  useEffect(() => {
+    localStorage.setItem("conversation_id", 0);
+  });
 
   function pushMessage(recipient, msg) {
     const newMessage = new Message({
       id: recipient,
-      message : msg,
+      message: msg,
       senderName: recipient,
     });
     setMessage([...message, newMessage]);
   }
 
   //const messages = message;
-  const messages = message.map((msg,id)=>{
-    return(
-      <Row >
-        <div className={"chat"+"-"+msg.id}>
-          {msg.id? <img className="chat-icon" src={Iris} alt="profile_pic" />:""}
-          {msg.id?"":<img className="chat-icon" src={user_icon} alt="profile_pic" />}
-          <ChatBubble  message={msg} />
+  const messages = message.map((msg, id) => {
+    return (
+      <Row>
+        <div className={"chat" + "-" + msg.id}>
+          {msg.id ? (
+            <img className="chat-icon" src={Iris} alt="profile_pic" />
+          ) : (
+            ""
+          )}
+          {msg.id ? (
+            ""
+          ) : (
+            <img className="chat-icon" src={user_icon} alt="profile_pic" />
+          )}
+          <ChatBubble message={msg} />
         </div>
       </Row>
-    )
-  })
-  
+    );
+  });
 
-  return(
+  return (
     <div className="chatbox">
       {messages}
       <div>
         <form id="message-form" onSubmit={onMessageSubmit}>
-          <input ref={m => input = m} id="input" placeholder="Type your message..."></input>
+          <input
+            ref={(m) => (input = m)}
+            id="input"
+            placeholder="Type your message..."
+          ></input>
           <img id="send-btn" onClick={onMessageSubmit} src={send}></img>
         </form>
       </div>
     </div>
-  )
+  );
 }
 
 export default Chatbox;
